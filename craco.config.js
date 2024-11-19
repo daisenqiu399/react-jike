f9bb3a38921eb33cff5c61ac30536637d3b960b7
戴森球 <3289990188@qq.com>
2024-11-19 18:04:04 +0800

    表单检验

--- craco.config.js
+++ craco.config.js
@@ -1,9 +1,12 @@
-import { resolve } from 'path'
+const path = require('path')
 
-export const webpack = {
-  // 配置别名
-  alias: {
-    // 约定：使用 @ 表示 src 文件所在路径
-    '@': resolve(__dirname, 'src')
+module.exports = {
+  // webpack 配置
+  webpack: {
+    // 配置别名
+    alias: {
+      // 约定：使用 @ 表示 src 文件所在路径
+      '@': path.resolve(__dirname, 'src')
+    }
   }
 }
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,4 +1,60 @@
-const Login=()=>{
-    return <div>this is login</div>
+import { Card } from 'antd'
+import logo from '@/assets/logo.png'
+import './index.scss'
+import { Form, Input, Button, Checkbox } from 'antd'
+
+
+const onFinish=(values)=>{
+ console.log(values)
 }
+const Login = () => {
+  return (
+    <div className="login">
+      <Card className="login-container">
+        <img className="login-logo" src={logo} alt="" />
+        {/* 登录表单 */}
+       {/* 失焦检验 */}
+        <Form onFinish={onFinish} validateTrigger="onBlur" >
+      <Form.Item
+      name="username"
+      rules={[
+        {
+          required: true,
+          message: '请输入手机号',
+        },
+        {
+          pattern:/^1[3-9]\d{9}$/,
+          message:'请输入正确的手机格式'
+        }
+      ]}
+      >
+        <Input size="large" placeholder="请输入手机号" />
+      </Form.Item>
+      <Form.Item
+        name="password"
+        rules={[
+          {
+            required: true,
+            message: '请输入验证码',
+          },
+        ]}
+      >
+        <Input size="large" placeholder="请输入验证码" />
+      </Form.Item>
+      <Form.Item>
+        <Checkbox className="login-checkbox-label">
+          我已阅读并同意「用户协议」和「隐私条款」
+        </Checkbox>
+      </Form.Item>
+      <Form.Item>
+        <Button type="primary" htmlType="submit" size="large" block>
+          登录
+        </Button>
+      </Form.Item>
+    </Form>
+      </Card>
+    </div>
+  )
+}
+
 export default Login
