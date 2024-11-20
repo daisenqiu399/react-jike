138c48961045cf57735f62e70a0b1dec48f7e50e
戴森球 <3289990188@qq.com>
2024-11-20 10:20:20 +0800

    使用token跳转

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,13 +1,27 @@
-import { Card } from 'antd'
+import { Card, message } from 'antd'
 import logo from '@/assets/logo.png'
 import './index.scss'
 import { Form, Input, Button, Checkbox } from 'antd'
+import { useDispatch } from 'react-redux'
+import { fetchLogin } from '@/store/modules/user'
+import { useNavigate } from 'react-router-dom'
 
 
-const onFinish=(values)=>{
- console.log(values)
-}
 const Login = () => {
+  const dispatch=useDispatch()
+  const navigate=useNavigate()
+  const onFinish=async (values)=>{
+    console.log(values)
+    
+    //触发异步action fetchLogin
+   await dispatch(fetchLogin(values))
+    //跳转到首页
+    navigate('/')
+
+    //提示一下用户
+    message.success('登录成功')
+
+   }
   return (
     <div className="login">
       <Card className="login-container">
@@ -16,7 +30,7 @@ const Login = () => {
        {/* 失焦检验 */}
         <Form onFinish={onFinish} validateTrigger="onBlur" >
       <Form.Item
-      name="username"
+      name="mobile"
       rules={[
         {
           required: true,
@@ -31,7 +45,7 @@ const Login = () => {
         <Input size="large" placeholder="请输入手机号" />
       </Form.Item>
       <Form.Item
-        name="password"
+        name="code"
         rules={[
           {
             required: true,
