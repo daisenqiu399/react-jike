a9357318b785163f7dca096fb53697b51af6f620
戴森球 <3289990188@qq.com>
2024-11-20 12:20:28 +0800

    Layout页面以及二级路由

--- src/router/index.js
+++ src/router/index.js
@@ -3,16 +3,39 @@ import Layout from "../pages/Layout";
 import Login from '@/pages/Login'
 import { AuthRoute } from "@/components/AuthRoute";
 
-import {createBrowserRouter} from 'react-router-dom'
+import { createBrowserRouter } from 'react-router-dom'
+import Publish from "@/pages/Publish";
+import Article from "@/pages/Article";
+import Home from "@/pages/Home";
 
-const router=createBrowserRouter([
+const router = createBrowserRouter([
     {
-        path:'/',
-        element:<AuthRoute><Layout/></AuthRoute>
+        path: '/',
+        element: <AuthRoute><Layout /></AuthRoute>,
+        children: [
+            {
+                //    path:'home',
+                // 默认二级路由首页
+                index: true,
+                element: <Home />
+            },
+
+            {
+                path: 'article',
+                element: <Article />
+            },
+
+            {
+                path: 'publish',
+                element: <Publish />
+            }
+        ]
+
+
     },
     {
-        path:'/login',
-        element:<Login/> 
+        path: '/login',
+        element: <Login />
     }
 
 ])
