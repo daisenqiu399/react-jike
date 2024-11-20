7a19e9bf5914da9b21d56d02f8294dbdfedb1d61
戴森球 <3289990188@qq.com>
2024-11-20 10:58:47 +0800

    axios注入token

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,5 +1,6 @@
 //axios封装
 import axios from 'axios'
+import { getToken } from './token'
 
 const request = axios.create({
   baseURL: 'http://geek.itheima.net/v1_0',
@@ -7,6 +8,13 @@ const request = axios.create({
 })
 // 添加请求拦截器
 request.interceptors.request.use((config)=> {
+  //操作这个config注入token数据
+  //获取到token
+  //按照后端的格式要求做token数据
+  const token=getToken()
+  if(token){
+    config.headers.Authorization=`Bearer ${token}`
+  }
     return config
   }, (error)=> {
     return Promise.reject(error)
