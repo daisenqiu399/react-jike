d2bf872bb793487d27f315711bfe2a7b41e1a1a0
戴森球 <3289990188@qq.com>
2024-11-20 10:47:42 +0800

    封装localstrage

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,20 +1,21 @@
 //和用户相关的状态管理
 import { createSlice } from "@reduxjs/toolkit";
 // import { userStore } from "react-redux";
-import { request } from "@/utils";
+import { request,setToken as _settoken,getToken } from "@/utils";
 
 const userStore=createSlice({
     name:"user",
     //数据状态
     initialState:{
-        token:localStorage.getItem('token_key')||''
+        token:getToken()||''
     },
     //同步修改方法
     reducers:{
         setToken(state,action){
             state.token=action.payload
             //localstorage也存一份
-            localStorage.setItem('token_key',action.payload)
+            // localStorage.setItem('token_key',action.payload)
+            _settoken(action.payload)
         }
     }
 })
